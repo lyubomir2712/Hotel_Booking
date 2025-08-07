6e6c5e3ba760c2b26afaaf6c659b643cae0d9bc7
Lyubomir Georgiev <lyubomirgeorgiev2712@gmail.com>
2025-08-07 22:23:14 +0300

    doing styling change to the app

--- HotelBookingApp/HotelBooking.Web/Controllers/HomeController.cs
+++ HotelBookingApp/HotelBooking.Web/Controllers/HomeController.cs
@@ -29,26 +29,22 @@ namespace HotelBooking.Web.Controllers
             _starsService = starsService;
             _dbContext = dbContext;
         }
-        
 
         public IActionResult Index()
         {
             return View();
         }
         
-
         public IActionResult Privacy()
         {
             return View();
         }
         
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        
         
         public IActionResult ErrorWithStatusCode(int errorCode)
         {
