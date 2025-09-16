37d1ce5040136f43cee52cf8b1d5f02752d433be
Lyubomir Georgiev <lyubomirgeorgiev2712@gmail.com>
2025-09-16 16:24:28 +0300

    added a test module for unit and integration tests

---
+++ HotelBookingApp/HotelBooking.Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace HotelBooking.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
