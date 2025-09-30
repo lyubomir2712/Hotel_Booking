9859f8fbc7315b6475492cc959dd3890fb0382c6
Lyubomir Georgiev <lyubomirgeorgiev2712@gmail.com>
2025-09-30 11:08:42 +0300

    added logger to getbookingsService

--- HotelBookingApp/HotelBooking.Services/HotelsServices/GetBookingsService.cs
+++ HotelBookingApp/HotelBooking.Services/HotelsServices/GetBookingsService.cs
@@ -2,19 +2,39 @@ using HotelBooking.Data;
 using HotelBooking.Data.SeedWork;
 using HotelBooking.Models.AppModels;
 using HotelBooking.Services.Contracts.HotelsServicesContracts;
+using HotelBooking.Services.Contracts.KafkaOperationsLoggerPublisherContracts;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelBooking.Services.HotelsServices;
 
 public class GetBookingsService : IGetBookingsService
 {
+    private readonly IKafkaOperationsLoggerProducer _logger;
+
+    public GetBookingsService(IKafkaOperationsLoggerProducer logger)
+    {
+        _logger = logger;
+    }
+
     public List<BookingModel> GetBookings(IUnitOfWork unitOfWork ,string userId)
     {
-            return unitOfWork.Repository<UserBookingModel>()
+            var bookings = unitOfWork.Repository<UserBookingModel>()
                 .Where(b => b.UserId == Convert.ToInt32(userId))
                 .Include(b => b.BookingModel)
                 .ThenInclude(b => b.HotelModel)
                 .Select(b => b.BookingModel)
                 .ToList();
+
+            foreach (var booking in bookings)
+            {
+                _logger.LogAsync(
+                    entityType: nameof(BookingModel),
+                    entityId: booking.Id.ToString(),
+                    operation: "Get",
+                    actorId: userId,
+                    source: nameof(GetBookingsService));
+            }
+
+            return bookings;
     }
 }
